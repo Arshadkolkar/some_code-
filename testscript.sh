8aa8bc677c3ccd22415327a37443381348aacf35
Ubuntu <kolkar.arshad@gamil.com>
2020-05-17 08:36:44 +0000

    autopull

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "This is an example of a script"
-echo "this is is running in arad "
+echo "this is is running in  "
 echo "This is param $PASSED"
 NUM=$(($FILSTNUMBER + $SECONNUMBER))
 echo "$NUM"
