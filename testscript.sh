c5a70de2f55f7bea9297b7ff0e33fabc78045d07
Ubuntu <kolkar.arshad@gamil.com>
2020-05-17 08:43:54 +0000

    autopull

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "This is an example of a script"
-echo "this is is running in  "
+echo "this is is runningin  "
 echo "This is param $PASSED"
 NUM=$(($FILSTNUMBER + $SECONNUMBER))
 echo "$NUM"
